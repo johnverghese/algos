6e72bec1c06b6b2565ccb76fa4d896c9fb63e02d
John Verghese <john1.verghese@gmail.com>
2017-09-15 11:14:48 -0400

    Radix sort works, with bucket sort underlying.

--- radixsort.py
+++ radixsort.py
@@ -1,21 +1,31 @@
+# This version of the algorithm is O(k(n + blogb)), which is slightly worse than
+# radix sort's ideal implementation: O(k(n + b)). I think that it's possible the
+# ease of implementing in a strongly typed language like python makes this
+# (using a dict) an attractive option, but this is worth revisiting.
 
 
-
+# works only for a list of integers of the same length
 def radixsort(a):
     a = [str(el) for el in a]
-    return bucketsort(a, 1)
+    #O(n)
+    #O(k(n + blogb) where k is string length)
+    for i in range(max([len(el) for el in a])-1, -1, -1):
+        a = bucketsort(a, i)
+    return a
 
+#O(n + blogb)
 def bucketsort(a, i):
-    chars = [int(el[i]) for el in a]
-    buckets = [[]] * (max(chars) + 1)
+    chars = [el[i] for el in a]
+    buckets = {}
     a_sorted = []
 
+    #O(n)
     for el in a:
-        el = int(el)
-        buckets[el[i]].append(el)
+        buckets.setdefault(el[i], []).append(el)
 
-    for i in range(len(buckets)):
-        a_sorted.extend(buckets[i])
+    #blogb where b is number of digit values
+    for key in sorted(buckets.keys()):
+        a_sorted.extend(buckets[key])
 
     return a_sorted
  
